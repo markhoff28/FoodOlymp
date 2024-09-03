539febd7bc23b022cb2d33af4c33a59bf8e2333f
markhoff28 <Magnus-Hoffmann@web.de>
2024-09-03 14:50:43 +0200

    [FO-3] Insert breadcrumb in admin profile

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -13,7 +13,7 @@
 
           <div class="page-title-right">
             <ol class="breadcrumb m-0">
-              <li class="breadcrumb-item"><a href="javascript: void(0);">Contacts</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}"><i class="bx bx-home-alt"></i></a></li>
               <li class="breadcrumb-item active">Profile</li>
             </ol>
           </div>
