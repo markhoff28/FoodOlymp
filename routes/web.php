b74a271312a3806139178dc143e14dda20ea9642
markhoff28 <Magnus-Hoffmann@web.de>
2024-09-14 12:14:35 +0200

    [FO-16] Admin backend product option setup part 4. Step 90

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,8 @@ Route::middleware('admin')->group(function () {
         Route::get('/admin/add/product', 'AdminAddProduct')->name('admin.add.product');
         Route::post('/admin/store/product', 'AdminStoreProduct')->name('admin.product.store');
         Route::get('/admin/edit/product/{id}', 'AdminEditProduct')->name('admin.edit.product');
+        Route::post('/admin/update/product', 'AdminUpdateProduct')->name('admin.product.update');
+        Route::get('/admin/delete/product/{id}', 'AdminDeleteProduct')->name('admin.delete.product');
         Route::get('/adminChangeStatus', 'AdminChangeStatus');
     });
 });
