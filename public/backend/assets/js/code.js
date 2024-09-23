e0f32823ecc0991218476cb656bf1a4cff2d5863
markhoff28 <Magnus-Hoffmann@web.de>
2024-09-23 18:37:48 +0200

    Fix code.js

--- public/backend/assets/js/code.js
+++ public/backend/assets/js/code.js
@@ -20,10 +20,8 @@ $(function () {
     });
 });
 
-/////////// confirm
-
 $(function () {
-    $(document).on("click", "#confirm", function (e) {
+    $(document).on("click", "#confirmOrder", function (e) {
         e.preventDefault();
         var link = $(this).attr("href");
 
@@ -39,6 +37,58 @@ $(function () {
             if (result.isConfirmed) {
                 window.location.href = link;
                 Swal.fire("Confirm!", "Your file has been Confirm.", "success");
+            }
+        });
+    });
+});
+
+$(function () {
+    $(document).on("click", "#processingOrder", function (e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Processing This Data?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Processing it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = link;
+                Swal.fire(
+                    "Processing!",
+                    "Your file has been Processing.",
+                    "success"
+                );
+            }
+        });
+    });
+});
+
+$(function () {
+    $(document).on("click", "#deliverdOrder", function (e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "deliverd This Data?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, deliverd it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = link;
+                Swal.fire(
+                    "deliverd!",
+                    "Your file has been deliverd.",
+                    "success"
+                );
             }
         });
     });
