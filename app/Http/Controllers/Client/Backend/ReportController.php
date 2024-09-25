d239fd68fa6e6362dda0b83bb5e59d46b1a05c50
markhoff28 <Magnus-Hoffmann@web.de>
2024-09-25 16:21:44 +0200

    [FO-33] Manage report system in restaurant panel part 2. Step 157

--- app/Http/Controllers/Client/Backend/ReportController.php
+++ app/Http/Controllers/Client/Backend/ReportController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Client\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
+use App\Models\OrderItem;
 use DateTime;
 
 class ReportController extends Controller
@@ -13,4 +15,24 @@ class ReportController extends Controller
     {
         return view('client.backend.report.all_report');
     } // End Method
+
+    public function ClientSearchByDate(Request $request)
+    {
+        $date = new DateTime($request->date);
+        $formatDate = $date->format('d F Y');
+        $cid = Auth::guard('client')->id();
+        $orders = Order::where('order_date', $formatDate)
+            ->whereHas('OrderItems', function ($query) use ($cid) {
+                $query->where('client_id', $cid);
+            })
+            ->latest()
+            ->get();
+        $orderItemGroupData = OrderItem::with(['order', 'product'])
+            ->whereIn('order_id', $orders->pluck('id'))
+            ->where('client_id', $cid)
+            ->orderBy('order_id', 'desc')
+            ->get()
+            ->groupBy('order_id');
+        return view('client.backend.report.search_by_date', compact('orderItemGroupData', 'formatDate'));
+    }  // End Method
 }
