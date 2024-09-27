7ee0c35c480e2605d94892fcb60177d88c0cdb34
markhoff28 <Magnus-Hoffmann@web.de>
2024-09-27 15:22:06 +0200

    [FO-34] Restaurant review system part 6. Step 164

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Client;
 use App\Models\Gallery;
+use App\Models\Review;
 use App\Models\Menu;
 
 class IndexController extends Controller
@@ -21,8 +22,24 @@ class IndexController extends Controller
         $menus = Menu::where('client_id', $client->id)->get()->filter(function ($menu) {
             return $menu->products->isNotEmpty();
         });
-        $gallerys = Gallery::where('client_id',$id)->get();
-        return view('frontend.details_page',compact('client','menus','gallerys'));
+        $gallerys = Gallery::where('client_id', $id)->get();
+        $reviews = Review::where('client_id', $client->id)->get();
+        $totalReviews = $reviews->count();
+        $ratingSum = $reviews->sum('rating');
+        $averageRating = $totalReviews > 0 ? $ratingSum / $totalReviews : 0;
+        $roundedAverageRating = round($averageRating, 1);
+
+        $ratingCounts = [
+            '5' => $reviews->where('rating', 5)->count(),
+            '4' => $reviews->where('rating', 4)->count(),
+            '3' => $reviews->where('rating', 3)->count(),
+            '2' => $reviews->where('rating', 2)->count(),
+            '1' => $reviews->where('rating', 1)->count(),
+        ];
+        $ratingPercentages =  array_map(function ($count) use ($totalReviews) {
+            return $totalReviews > 0 ? ($count / $totalReviews) * 100 : 0;
+        }, $ratingCounts);
+        return view('frontend.details_page', compact('client', 'menus', 'gallerys', 'reviews', 'roundedAverageRating', 'totalReviews', 'ratingCounts', 'ratingPercentages'));
     }
     //End Method
 }
