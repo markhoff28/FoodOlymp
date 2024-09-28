fc285c4325bb9577024bbc9798946191c16194e9
markhoff28 <Magnus-Hoffmann@web.de>
2024-09-28 07:57:53 +0200

    Cleanup imports 2

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -4,13 +4,8 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Product;
-use Illuminate\Support\Facades\Session;
-use App\Models\Coupon;
 use App\Models\Order;
 use App\Models\OrderItem;
-use Carbon\Carbon;
 
 class OrderController extends Controller
 {
