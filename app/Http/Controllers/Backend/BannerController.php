c8c23def93019221760b4cb1b896002464a5db21
markhoff28 <Magnus-Hoffmann@web.de>
2024-12-06 22:36:07 +0100

    Format code

--- app/Http/Controllers/Backend/BannerController.php
+++ app/Http/Controllers/Backend/BannerController.php
@@ -41,7 +41,8 @@ class BannerController extends Controller
         return redirect()->back()->with($notification);
     } // End Method
 
-    public function EditBanner($id){
+    public function EditBanner($id)
+    {
         $banner = Banner::find($id);
         if ($banner) {
             $banner->banner_image = asset($banner->banner_image);
@@ -49,46 +50,45 @@ class BannerController extends Controller
         return response()->json($banner);
     } // End Method
 
-    public function BannerUpdate(Request $request){
+    public function BannerUpdate(Request $request)
+    {
 
         $banner_id = $request->banner_id;
 
         if ($request->file('image')) {
             $image = $request->file('image');
             $manager = new ImageManager(new Driver());
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
             $img = $manager->read($image);
-            $img->resize(400,400)->save(public_path('upload/banner/'.$name_gen));
-            $save_url = 'upload/banner/'.$name_gen;
+            $img->resize(400, 400)->save(public_path('upload/banner/' . $name_gen));
+            $save_url = 'upload/banner/' . $name_gen;
 
             Banner::find($banner_id)->update([
                 'banner_url' => $request->url,
-                'banner_image' => $save_url, 
-            ]); 
+                'banner_image' => $save_url,
+            ]);
             $notification = array(
                 'message' => 'Banner Updated Successfully',
                 'alert-type' => 'success'
             );
 
             return redirect()->route('all.banner')->with($notification);
-
         } else {
 
             Banner::find($banner_id)->update([
-                'banner_url' => $request->url, 
-            ]); 
+                'banner_url' => $request->url,
+            ]);
             $notification = array(
                 'message' => 'Banner Updated Successfully',
                 'alert-type' => 'success'
             );
 
             return redirect()->route('all.banner')->with($notification);
-
         }
-
     } // End Method 
 
-    public function DeleteBanner($id){
+    public function DeleteBanner($id)
+    {
         $item = Banner::find($id);
         $img = $item->banner_image;
         unlink($img);
@@ -101,6 +101,5 @@ class BannerController extends Controller
         );
 
         return redirect()->back()->with($notification);
-
     } // End Method
 }
